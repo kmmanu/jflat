ee32a4ad35bff0c76f0fd3c797c5aaf66e15cb59
dimitri.papaioannou <dimitri.papaioannou@37fb59ec-bb7c-11de-b528-055f0b42e881>
2009-10-19 17:43:59 +0000

    - fixed reports task - added unit tests

--- src/java/com/tecacet/jflat/util/conversion/ToStringConverter.java
+++ src/java/com/tecacet/jflat/util/conversion/ToStringConverter.java
@@ -1,8 +1,9 @@
 package com.tecacet.jflat.util.conversion;
 
 /**
- * Type converter that converts a type to String
+ * Type converter that converts a type to String.
  * 
+ * @param T the type to be converted to String
  */
 public interface ToStringConverter<T> {
 
