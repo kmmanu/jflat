9d772adaf945b73cb57ef62eb3b20dd23d5d6e18
dimitri.papaioannou <dimitri.papaioannou@37fb59ec-bb7c-11de-b528-055f0b42e881>
2010-01-20 19:03:19 +0000

    - fixed new line for writer - fixed fialing tests

--- trunk/src/test/com/tecacet/jflat/FlatFileReaderCallbackTest.java
+++ trunk/src/test/com/tecacet/jflat/FlatFileReaderCallbackTest.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.tecacet.jflat.om.Customer;
 import com.tecacet.jflat.om.Order;
 
 public class FlatFileReaderCallbackTest {
@@ -26,14 +27,15 @@ public class FlatFileReaderCallbackTest {
                 String[] name = tokens[1].split(",");
                 String lastName = name[0];
                 String firstName = name[1];
+                order.setCustomer(new Customer());
                 order.getCustomer().setLastName(lastName);
                 order.getCustomer().setFirstName(firstName);
                 orders.add(order);
             }
         };
 
-        ReaderRowMapper<Order> rowMapper = new BeanReaderRowMapper<Order>(Order.class, new String[] { "number",
-               "price"}, new String[] { "Number", "Price" });
+        ReaderRowMapper<Order> rowMapper = new BeanReaderRowMapper<Order>(Order.class,
+                new String[] { "number", "price" }, new String[] { "Number", "Price" });
         FileReader reader = new FileReader("testdata/orders.csv");
         CSVReader<Order> csvReader = new CSVReader<Order>(reader, rowMapper);
         csvReader.readWithCallback(callback);
