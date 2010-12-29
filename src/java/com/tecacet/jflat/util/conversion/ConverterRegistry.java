0343915fc8ea078d3494ce7e4180f2ce9b8af220
dimitri.papaioannou <dimitri.papaioannou@37fb59ec-bb7c-11de-b528-055f0b42e881>
2010-12-29 19:46:05 +0000

    generalizing conversion utilities

---
+++ src/java/com/tecacet/jflat/util/conversion/ConverterRegistry.java
@@ -0,0 +1,14 @@
+package com.tecacet.jflat.util.conversion;
+
+import java.util.Map;
+
+import org.apache.commons.beanutils.Converter;
+
+public interface ConverterRegistry {
+
+	void registerConverter(Class type, Converter converter);
+	
+	void deregister(Class type);
+	
+	Map<Class, Converter> getRegisteredConverters();
+}
