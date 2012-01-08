4294f548facaf6baeaec2df31cacf1da4f190523
dimitri.papaioannou <dimitri.papaioannou@gmail.com>
2012-01-08 04:55:24 +0000

    added common interface for file readers

--- src/test/com/tecacet/jflat/StockPriceReaderTest.java
+++ src/test/com/tecacet/jflat/StockPriceReaderTest.java
@@ -14,11 +14,23 @@ import org.apache.commons.beanutils.converters.DateConverter;
 import org.junit.Test;
 
 import com.tecacet.jflat.om.StockPrice;
+import com.tecacet.util.conversion.ConverterRegistry;
+import com.tecacet.util.conversion.GenericConverterRegistry;
 
 public class StockPriceReaderTest {
 
     @Test
     public void testSimpleRead() throws IOException, LineMergerException {
+
+        //TODO Problem: converters must be registered BEFORE BeanReaderRowMapper is initialized
+        
+        // The following code registers a converted with Commons Beans so that
+        // it knows how to create dates from string
+        // (required commons-beanutils-1.8)
+        DateConverter dateConverter = new DateConverter(null);
+        dateConverter.setPattern("yyyy-MM-dd");
+        ConverterRegistry converterRegistry = GenericConverterRegistry.getInstance();
+        converterRegistry.registerConverter(Date.class, dateConverter);
 
         // construct a bean row mapper for the LehmnaPrice object using the map
         ReaderRowMapper<StockPrice> rowMapper = new BeanReaderRowMapper<StockPrice>(StockPrice.class, new String[] {
@@ -28,13 +40,6 @@ public class StockPriceReaderTest {
         FileReader fr = new FileReader("testdata/prices.csv");
         // finally a CSV reader
         CSVReader<StockPrice> csvReader = new CSVReader<StockPrice>(fr, rowMapper);
-
-        // The following code registers a converted with Commons Beans so that
-        // it knows how to create dates from string
-        // (required commons-beanutils-1.8)
-        DateConverter dateConverter = new DateConverter(null);
-        dateConverter.setPattern("yyyy-MM-dd");
-        ConvertUtils.register(dateConverter, Date.class);
 
         // read all the prices
         List<StockPrice> prices = csvReader.readAll();
@@ -64,7 +69,7 @@ public class StockPriceReaderTest {
         price = prices.get(0);
 
         assertEquals(1521220000, price.getVolume());
-        
+
         new File("test.out").delete();
 
     }
