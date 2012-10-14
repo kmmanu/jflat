fd39c05e81c287fcf1424d56e95c6f86e5e0a3ef
dimitri.papaioannou@gmail.com <dimitri.papaioannou@gmail.com@37fb59ec-bb7c-11de-b528-055f0b42e881>
2012-10-14 17:55:37 +0000

    property-specific converters

--- trunk/src/main/java/com/tecacet/jflat/BeanReaderRowMapper.java
+++ trunk/src/main/java/com/tecacet/jflat/BeanReaderRowMapper.java
@@ -16,7 +16,10 @@
 
 package com.tecacet.jflat;
 
+import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.beanutils.Converter;
 
 import com.tecacet.util.introspection.BeanFactory;
 import com.tecacet.util.introspection.BeanIntrospectorException;
@@ -36,113 +39,131 @@ import com.tecacet.util.introspection.spring.SpringBeanWrapperPropertyAccessor;
  */
 public class BeanReaderRowMapper<T> implements ReaderRowMapper<T> {
 
-    private final int headerRow;
+	private final int headerRow;
 
-    private ColumnMapping columnMapping;
-    private PropertyAccessor<T> propertyAccessor;
-    private BeanFactory beanFactory = new DefaultBeanFactory();
-    private Class<T> type;
+	private ColumnMapping columnMapping;
+	private PropertyAccessor<T> propertyAccessor;
+	private BeanFactory beanFactory = new DefaultBeanFactory();
+	private Class<T> type;
 
-    /**
-     * Construct a rowMapper using a BeanManipulator to create beans and
-     * populate properties and a column mapping to determine which properties to
-     * map to columns
-     * 
-     * @param beanManipulator
-     * @param columnMapping
-     */
-    public BeanReaderRowMapper(Class<T> type, PropertyAccessor<T> propertyAccessor, ColumnMapping columnMapping) {
-        this.columnMapping = columnMapping;
-        this.propertyAccessor = propertyAccessor;
-        this.type = type;
-        this.headerRow = 1;
-    }
-    
-    public BeanReaderRowMapper(Class<T> type, PropertyAccessor<T> propertyAccessor, ColumnMapping columnMapping, int headerRow) {
-        this.columnMapping = columnMapping;
-        this.propertyAccessor = propertyAccessor;
-        this.type = type;
-        this.headerRow = headerRow;
-    }
+	private Map<String, Converter> converters = new HashMap<String, Converter>();
 
-    /**
-     * Construct a rowMapper using a the default BeanManipulator and a column
-     * mapping to determine which properties to map to columns
-     * 
-     * @param type
-     * @param columnMapping
-     */
-    public BeanReaderRowMapper(Class<T> type, ColumnMapping columnMapping) {
-        this(type, new SpringBeanWrapperPropertyAccessor<T>(), columnMapping);
-    }
+	/**
+	 * Construct a rowMapper using a BeanManipulator to create beans and
+	 * populate properties and a column mapping to determine which properties to
+	 * map to columns
+	 * 
+	 * @param beanManipulator
+	 * @param columnMapping
+	 */
+	public BeanReaderRowMapper(Class<T> type,
+			PropertyAccessor<T> propertyAccessor, ColumnMapping columnMapping) {
+		this.columnMapping = columnMapping;
+		this.propertyAccessor = propertyAccessor;
+		this.type = type;
+		this.headerRow = 1;
+	}
 
-    /**
-     * Construct a rowMapper using a the default BeanManipulator The mapping
-     * strategy is HeaderColumnNameMapping.
-     * 
-     * @param type
-     * @param map
-     *            A map from column names to properties
-     */
-    public BeanReaderRowMapper(Class<T> type, Map<String, String> columns) {
-        this(type, new SpringBeanWrapperPropertyAccessor<T>(), new HeaderColumnNameMapping(columns));
-    }
+	public BeanReaderRowMapper(Class<T> type,
+			PropertyAccessor<T> propertyAccessor, ColumnMapping columnMapping,
+			int headerRow) {
+		this.columnMapping = columnMapping;
+		this.propertyAccessor = propertyAccessor;
+		this.type = type;
+		this.headerRow = headerRow;
+	}
 
-    public BeanReaderRowMapper(Class<T> type, String[] properties, String[] header) {
-        this(type, new SpringBeanWrapperPropertyAccessor<T>(), new HeaderColumnNameMapping(properties, header));
-    }
+	/**
+	 * Construct a rowMapper using a the default BeanManipulator and a column
+	 * mapping to determine which properties to map to columns
+	 * 
+	 * @param type
+	 * @param columnMapping
+	 */
+	public BeanReaderRowMapper(Class<T> type, ColumnMapping columnMapping) {
+		this(type, new SpringBeanWrapperPropertyAccessor<T>(), columnMapping);
+	}
 
-    /**
-     * Returns a bean constructed and populated using the BeanManipulator.
-     * WARNING: This implementation returns null for the header row if the
-     * ColumnMapping requires a header row.
-     */
-    @SuppressWarnings("unchecked")
-    @Override
-    public T getRow(String[] row, int rowNumber) throws BeanIntrospectorException {
-        if (rowNumber == headerRow) {
-            if (columnMapping.requiresHeaderRow()) {
-                columnMapping.setHeaderRow(row);
-                return null;
-            }
-        }
-        T bean = (T) beanFactory.createBean(type);
-        for (int i = 0; i < row.length; i++) {
-            String property = columnMapping.getProperty(i);
-            if (property == null) {
-                continue;
-            }
-            propertyAccessor.setProperty(bean, property, row[i]);
-        }
-        return bean;
-    }
+	/**
+	 * Construct a rowMapper using a the default BeanManipulator The mapping
+	 * strategy is HeaderColumnNameMapping.
+	 * 
+	 * @param type
+	 * @param map
+	 *            A map from column names to properties
+	 */
+	public BeanReaderRowMapper(Class<T> type, Map<String, String> columns) {
+		this(type, new SpringBeanWrapperPropertyAccessor<T>(),
+				new HeaderColumnNameMapping(columns));
+	}
 
-    public ColumnMapping getColumnMapping() {
-        return columnMapping;
-    }
+	public BeanReaderRowMapper(Class<T> type, String[] properties,
+			String[] header) {
+		this(type, new SpringBeanWrapperPropertyAccessor<T>(),
+				new HeaderColumnNameMapping(properties, header));
+	}
 
-    public void setColumnMapping(ColumnMapping columnMapping) {
-        this.columnMapping = columnMapping;
-    }
+	/**
+	 * Returns a bean constructed and populated using the BeanManipulator.
+	 * WARNING: This implementation returns null for the header row if the
+	 * ColumnMapping requires a header row.
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public T getRow(String[] row, int rowNumber)
+			throws BeanIntrospectorException {
+		if (rowNumber == headerRow) {
+			if (columnMapping.requiresHeaderRow()) {
+				columnMapping.setHeaderRow(row);
+				return null;
+			}
+		}
+		T bean = (T) beanFactory.createBean(type);
+		for (int i = 0; i < row.length; i++) {
+			String property = columnMapping.getProperty(i);
+			if (property == null) {
+				continue;
+			}
+			Converter converter = converters.get(property);
+			if (converter != null) {
+				Object value = converter.convert(null, row[i]);
+				propertyAccessor.setProperty(bean, property, value);
+			} else {
+				propertyAccessor.setProperty(bean, property, row[i]);
+			}
+		}
+		return bean;
+	}
 
-    public int getHeaderRow() {
-        return headerRow;
-    }
+	public ColumnMapping getColumnMapping() {
+		return columnMapping;
+	}
 
-    public BeanFactory getBeanFactory() {
-        return beanFactory;
-    }
+	public void setColumnMapping(ColumnMapping columnMapping) {
+		this.columnMapping = columnMapping;
+	}
 
-    public void setBeanFactory(BeanFactory beanFactory) {
-        this.beanFactory = beanFactory;
-    }
+	public int getHeaderRow() {
+		return headerRow;
+	}
 
-    public PropertyAccessor<T> getPropertyAccessor() {
-        return propertyAccessor;
-    }
+	public BeanFactory getBeanFactory() {
+		return beanFactory;
+	}
 
-    public void setPropertyAccessor(PropertyAccessor<T> propertyAccessor) {
-        this.propertyAccessor = propertyAccessor;
-    }
+	public void setBeanFactory(BeanFactory beanFactory) {
+		this.beanFactory = beanFactory;
+	}
 
+	public PropertyAccessor<T> getPropertyAccessor() {
+		return propertyAccessor;
+	}
+
+	public void setPropertyAccessor(PropertyAccessor<T> propertyAccessor) {
+		this.propertyAccessor = propertyAccessor;
+	}
+
+	public void registerConverter(String property, Converter converter) {
+		converters.put(property, converter);
+	}
 }
